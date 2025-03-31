c26c5eb73fca39c0b743f1cc872e932e499c1789
Naphat <gobossza35@gmail.com>
2025-03-31 19:00:16 +0700

    Update
    - Add dashboard authorization
    - Edit ads export dto

--- app/controllers/registrations_controller.ts
+++ app/controllers/registrations_controller.ts
@@ -1,9 +1,15 @@
+import { isAccess } from '#abilities/main'
+import app from '#config/app'
 import registration_service from '#services/registration_service'
 import { registrationValidator } from '#validators/registration'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class RegistrationsController {
-    async getGetNumberOfRegisAds({ request, response }: HttpContext) {
+    private dashboardActivityId = 3
+
+    async getGetNumberOfRegisAds({ request, response, bouncer }: HttpContext) {
+        await bouncer.authorize(isAccess, app.defaultView, this.dashboardActivityId)
+
         const status = request.input('status') || null
         const orderField = request.input('orderField') || null
         const orderType = request.input('orderType') || null
