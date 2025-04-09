6020abbde72382370e188836bf2325cf5c7ab942
Naphat <gobossza35@gmail.com>
2025-04-10 01:13:35 +0700

    Update chart_service.ts
    add try catch

--- app/services/chart_service.ts
+++ app/services/chart_service.ts
@@ -1,3 +1,4 @@
+import HandlerException from "#exceptions/handler_exception"
 import Advertisement from "#models/advertisement"
 import Plant from "#models/plant"
 import Registration from "#models/registration"
@@ -6,50 +7,62 @@ import time_service from "./time_service.js"
 import db from "@adonisjs/lucid/services/db"
 
 const getAdsGroupStatus = async () => {
-    const ads = await Advertisement.query()
-        .select('status')
-        .whereNotNull('status')
-        .where('status', '!=', '')
-        .count('* as count')
-        .groupBy('status')
-        .orderBy('status', 'asc')
+    try {
+        const ads = await Advertisement.query()
+            .select('status')
+            .whereNotNull('status')
+            .where('status', '!=', '')
+            .count('* as count')
+            .groupBy('status')
+            .orderBy('status', 'asc')
 
-    const adsDTO = ads.map((ad) => {
-        return new AdsGrupStatusDTO(ad, ad.$extras.count)
-    })
-    return adsDTO
+        const adsDTO = ads.map((ad) => {
+            return new AdsGrupStatusDTO(ad, ad.$extras.count)
+        })
+        return adsDTO
+    } catch (error) {
+        throw new HandlerException(error.status, error.message)
+    }
 }
 
 const getAdsGroupPeriod = async () => {
-    const ads = await Advertisement.query()
-        .select('periodId')
-        .whereNotNull('periodId')
-        .where('periodId', '!=', 0)
-        .preload('period')
-        .count('* as count')
-        .groupBy('periodId')
-        .orderBy('periodId', 'asc')
+    try {
+        const ads = await Advertisement.query()
+            .select('periodId')
+            .whereNotNull('periodId')
+            .where('periodId', '!=', 0)
+            .preload('period')
+            .count('* as count')
+            .groupBy('periodId')
+            .orderBy('periodId', 'asc')
 
-    const adsDTO = ads.map((ad) => {
-        return new AdsGroupPeriodDTO(ad, ad.$extras.count)
-    })
-    return adsDTO
+        const adsDTO = ads.map((ad) => {
+            return new AdsGroupPeriodDTO(ad, ad.$extras.count)
+        })
+        return adsDTO
+    } catch (error) {
+        throw new HandlerException(error.status, error.message)
+    }
 }
 
 const getAdsGroupPackage = async () => {
-    const ads = await Advertisement.query()
-        .select('packageId')
-        .whereNotNull('packageId')
-        .where('packageId', '!=', 0)
-        .preload('packages')
-        .count('* as count')
-        .groupBy('packageId')
-        .orderBy('packageId', 'asc')
+    try {
+        const ads = await Advertisement.query()
+            .select('packageId')
+            .whereNotNull('packageId')
+            .where('packageId', '!=', 0)
+            .preload('packages')
+            .count('* as count')
+            .groupBy('packageId')
+            .orderBy('packageId', 'asc')
 
-    const adsDTO = ads.map((ad) => {
-        return new AdsGroupPackageDTO(ad, ad.$extras.count)
-    })
-    return adsDTO
+        const adsDTO = ads.map((ad) => {
+            return new AdsGroupPackageDTO(ad, ad.$extras.count)
+        })
+        return adsDTO
+    } catch (error) {
+        throw new HandlerException(error.status, error.message)
+    }
 }
 
 const getTopRegisByPlant = async (
@@ -59,94 +72,98 @@ const getTopRegisByPlant = async (
     quarter: number | string | null = null,
     limit: number = 10
 ) => {
-    time_service.validateYearAndQuarter(year, quarter)
+    try {
+        time_service.validateYearAndQuarter(year, quarter)
 
-    let monthYearStart: string
-    let monthYearEnd: string
-    if (year && quarter) {
-        monthYearStart = year + '-' + time_service.convertQuarterToMonth(Number(quarter)).start
-        monthYearEnd = year + '-' + time_service.convertQuarterToMonth(Number(quarter)).end
+        let monthYearStart: string
+        let monthYearEnd: string
+        if (year && quarter) {
+            monthYearStart = year + '-' + time_service.convertQuarterToMonth(Number(quarter)).start
+            monthYearEnd = year + '-' + time_service.convertQuarterToMonth(Number(quarter)).end
+        }
+
+        const plant = await Plant.query()
+            .whereHas('tambon', (tambonQuery) => {
+                tambonQuery.whereHas('amphure', (amphureQuery) => {
+                    amphureQuery.whereHas('province', (provinceQuery) => {
+                        provinceQuery.if(provinceId, (query) => query.where('id', provinceId!))
+                            .whereHas('geography', (geographyQuery) => {
+                                geographyQuery.if(geographyId, (query) => query.where('id', geographyId!))
+                            })
+                    })
+                })
+            })
+            .preload('tambon', (tambonQuery) => {
+                tambonQuery.preload('amphure', (amphureQuery) => {
+                    amphureQuery.preload('province', (provinceQuery) => {
+                        provinceQuery.preload('geography')
+                    })
+                })
+            })
+            // Filter plant
+            .if(year && quarter, (query) => {
+                query.whereHas('registration', (regQuery) => {
+                    regQuery.whereHas('advertisement', (adQuery) => {
+                        adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') >= ?`, [monthYearStart as string])
+                            .whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') <= ?`, [monthYearEnd as string])
+                    })
+                })
+            })
+            .if(year && !quarter, (query) => {
+                query.whereHas('registration', (regQuery) => {
+                    regQuery.whereHas('advertisement', (adQuery) => {
+                        adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy') = ?`, [year!.toString() as string])
+                    })
+                })
+            })
+            // Preload with filter
+            .preload('registration', (regQuery) => {
+                regQuery.whereHas('advertisement', (adQuery) => {
+                    if (year && quarter) {
+                        adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') >= ?`, [monthYearStart as string])
+                            .whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') <= ?`, [monthYearEnd as string])
+                    }
+                    if (year && !quarter) {
+                        adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy') = ?`, [year.toString() as string])
+                    }
+                })
+                regQuery.preload('advertisement')
+            })
+            // Count with filter
+            .withCount('registration', (regQuery) => {
+                regQuery.whereHas('advertisement', (adQuery) => {
+                    if (year && quarter) {
+                        adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') >= ?`, [monthYearStart as string])
+                            .whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') <= ?`, [monthYearEnd as string])
+                    }
+                    if (year && !quarter) {
+                        adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy') = ?`, [year.toString() as string])
+                    }
+                })
+                regQuery.as('totalRegistration')
+            })
+
+            .orderBy('totalRegistration', 'desc')
+            .limit(limit)
+
+        const plantDTO = plant.map((pla) => {
+            return new TopPlantDTO(pla,
+                pla.tambon.amphure.province.geography,
+                pla.tambon.amphure.province,
+                pla.tambon.amphure,
+                pla.tambon,
+                pla.$extras.totalRegistration
+            )
+        })
+
+        plantDTO.forEach((pla) => {
+            pla.regis = groupRegistrationsByAdsId(pla.regis)
+        })
+
+        return plantDTO
+    } catch (error) {
+        throw new HandlerException(error.status, error.message)
     }
-
-    const plant = await Plant.query()
-        .whereHas('tambon', (tambonQuery) => {
-            tambonQuery.whereHas('amphure', (amphureQuery) => {
-                amphureQuery.whereHas('province', (provinceQuery) => {
-                    provinceQuery.if(provinceId, (query) => query.where('id', provinceId!))
-                        .whereHas('geography', (geographyQuery) => {
-                            geographyQuery.if(geographyId, (query) => query.where('id', geographyId!))
-                        })
-                })
-            })
-        })
-        .preload('tambon', (tambonQuery) => {
-            tambonQuery.preload('amphure', (amphureQuery) => {
-                amphureQuery.preload('province', (provinceQuery) => {
-                    provinceQuery.preload('geography')
-                })
-            })
-        })
-        // Filter plant
-        .if(year && quarter, (query) => {
-            query.whereHas('registration', (regQuery) => {
-                regQuery.whereHas('advertisement', (adQuery) => {
-                    adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') >= ?`, [monthYearStart as string])
-                        .whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') <= ?`, [monthYearEnd as string])
-                })
-            })
-        })
-        .if(year && !quarter, (query) => {
-            query.whereHas('registration', (regQuery) => {
-                regQuery.whereHas('advertisement', (adQuery) => {
-                    adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy') = ?`, [year!.toString() as string])
-                })
-            })
-        })
-        // Preload with filter
-        .preload('registration', (regQuery) => {
-            regQuery.whereHas('advertisement', (adQuery) => {
-                if (year && quarter) {
-                    adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') >= ?`, [monthYearStart as string])
-                        .whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') <= ?`, [monthYearEnd as string])
-                }
-                if (year && !quarter) {
-                    adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy') = ?`, [year.toString() as string])
-                }
-            })
-            regQuery.preload('advertisement')
-        })
-        // Count with filter
-        .withCount('registration', (regQuery) => {
-            regQuery.whereHas('advertisement', (adQuery) => {
-                if (year && quarter) {
-                    adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') >= ?`, [monthYearStart as string])
-                        .whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') <= ?`, [monthYearEnd as string])
-                }
-                if (year && !quarter) {
-                    adQuery.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy') = ?`, [year.toString() as string])
-                }
-            })
-            regQuery.as('totalRegistration')
-        })
-
-        .orderBy('totalRegistration', 'desc')
-        .limit(limit)
-
-    const plantDTO = plant.map((pla) => {
-        return new TopPlantDTO(pla,
-            pla.tambon.amphure.province.geography,
-            pla.tambon.amphure.province,
-            pla.tambon.amphure,
-            pla.tambon,
-            pla.$extras.totalRegistration
-        )
-    })
-
-    plantDTO.forEach((pla) => {
-        pla.regis = groupRegistrationsByAdsId(pla.regis)
-    })
-
-    return plantDTO
 }
 
 const getTopRegisByAds = async (
@@ -157,60 +174,68 @@ const getTopRegisByAds = async (
     quarter: number | string | null = null,
     limit: number = 10
 ) => {
-    time_service.validateYearAndQuarter(year, quarter)
+    try {
+        time_service.validateYearAndQuarter(year, quarter)
 
-    let monthYearStart: string
-    let monthYearEnd: string
-    if (year && quarter) {
-        monthYearStart = year + '-' + time_service.convertQuarterToMonth(Number(quarter)).start
-        monthYearEnd = year + '-' + time_service.convertQuarterToMonth(Number(quarter)).end
+        let monthYearStart: string
+        let monthYearEnd: string
+        if (year && quarter) {
+            monthYearStart = year + '-' + time_service.convertQuarterToMonth(Number(quarter)).start
+            monthYearEnd = year + '-' + time_service.convertQuarterToMonth(Number(quarter)).end
+        }
+
+        const ads = await Advertisement.query()
+            .if(periodId, (query) => query.where('periodId', periodId!))
+            .if(packageId, (query) => query.where('packageId', packageId!))
+            .if(status, (query) => query.where('status', status!))
+            .if(!status, (query) => query.whereIn('status', ['A', 'N']))
+            .if(year && quarter, (query) => {
+                query.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') >= ?`, [monthYearStart as string])
+                    .whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') <= ?`, [monthYearEnd as string])
+            })
+            .if(year && !quarter, (query) => {
+                query.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy') = ?`, [year!.toString() as string])
+            })
+            .preload('period')
+            .preload('packages')
+            .preload('registrations')
+            .withCount('registrations', (query) => query.as('totalRegistration'))
+            .orderBy('totalRegistration', 'desc')
+            .limit(limit)
+
+        const adsDTO = ads.map((ad) => {
+            return new TopAdsDTO(ad, ad.$extras.totalRegistration)
+        })
+
+        return adsDTO
+    } catch (error) {
+        throw new HandlerException(error.status, error.message)
     }
-
-    const ads = await Advertisement.query()
-        .if(periodId, (query) => query.where('periodId', periodId!))
-        .if(packageId, (query) => query.where('packageId', packageId!))
-        .if(status, (query) => query.where('status', status!))
-        .if(!status, (query) => query.whereIn('status', ['A', 'N']))
-        .if(year && quarter, (query) => {
-            query.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') >= ?`, [monthYearStart as string])
-                .whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy-MM') <= ?`, [monthYearEnd as string])
-        })
-        .if(year && !quarter, (query) => {
-            query.whereRaw(`FORMAT(RGS_STR_DATE, 'yyyy') = ?`, [year!.toString() as string])
-        })
-        .preload('period')
-        .preload('packages')
-        .preload('registrations')
-        .withCount('registrations', (query) => query.as('totalRegistration'))
-        .orderBy('totalRegistration', 'desc')
-        .limit(limit)
-
-    const adsDTO = ads.map((ad) => {
-        return new TopAdsDTO(ad, ad.$extras.totalRegistration)
-    })
-
-    return adsDTO
 }
 
 const getRegisPerMonthByAds = async (adsId: number) => {
-    const ads = await Advertisement.query()
-        .where('adsId', adsId)
-        .preload('period')
-        .preload('packages')
-        .firstOrFail()
+    try {
+        const ads = await Advertisement.query()
+            .where('adsId', adsId)
+            .preload('period')
+            .preload('packages')
+            .firstOrFail()
 
-    const regisByAds = await Registration.query()
-        .where('adsId', adsId)
-        .select(
-            db.raw("FORMAT(REGIS_DATE, 'yyyy-MM') as regisMonth"),
-            db.raw('COUNT(*) as count')
-        )
-        .groupByRaw("FORMAT(REGIS_DATE, 'yyyy-MM')")
-        .orderBy('regisMonth')
+        const regisByAds = await Registration.query()
+            .where('adsId', adsId)
+            .select(
+                db.raw("FORMAT(REGIS_DATE, 'yyyy-MM') as regisMonth"),
+                db.raw('COUNT(*) as count')
+            )
+            .groupByRaw("FORMAT(REGIS_DATE, 'yyyy-MM')")
+            .orderBy('regisMonth')
 
-    const result = new RegisPerMonthByAdsDTO(ads, regisByAds.map((reg) => reg.$extras))
+        const result = new RegisPerMonthByAdsDTO(ads, regisByAds.map((reg) => reg.$extras))
 
-    return result
+        return result
+    } catch (error) {
+        throw new HandlerException(error.status, error.message)
+    }
 }
 
 interface OldRegisForm {
